4d280f69f518c955d7a9b17708cbac662d86a253
Shan <lxxshan33@gmail.comm>
2024-01-20 15:17:00 -0500

    Revert wrong push to main

--- config/database.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-mongoose.connect(process.env.DATABASE_URI)
-
-const db = mongoose.connection
-db.on('connected', function(){
-    console.log(`Connected to MongoDB ${db.name} at ${db.host}`)
-})
